6aa542cbbd563359486e9f3ac922b92d1c5941a7
renzlaurennn <renzlaurennn@gmail.com>
2023-07-10 10:27:58 +0800

    update

---
+++ email.js
@@ -0,0 +1,32 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = (crashType) => {
+    // Create a transporter using SMTP or other transport options
+    const transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        user: 'pd2bikebox@gmail.com', // Include the full email address
+        pass: 'ouwyxrvdmgsykwzt'
+      }
+    });
+  
+    // Define the email message
+    const mailOptions = {
+      from: 'pd2bikebox@gmail.com', // Include the full email address
+      to: 'renzlaurennn@gmail.com',
+      subject: 'Crash Alert',
+      text: `A ${crashType} crash has been detected.`
+    };
+  
+    // Send the email
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log('Error:', error);
+      } else {
+        console.log('Email sent:', info.response);
+      }
+    });
+  };
+  
+  module.exports = { sendEmail };
+  
