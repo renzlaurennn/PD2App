29b1c273b627003a04fe3f9cb9ed3682731debc9
starkfire <plpantoja49@gmail.com>
2023-07-14 11:53:32 +0800

    fix: export firebase config directly

--- firebase.js
+++ firebase.js
@@ -1,8 +1,8 @@
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, onValue } from "firebase/database";
+import { getDatabase, ref } from "firebase/database";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
+export const firebaseApp = initializeApp({
   apiKey: "AIzaSyD15aHe9nyzRZxyxXvyuGizy4wZxXTcMXs",
   authDomain: "bikebox-app.firebaseapp.com",
   databaseURL: "https://bikebox-app-default-rtdb.asia-southeast1.firebasedatabase.app",
@@ -10,10 +10,9 @@ const firebaseConfig = {
   storageBucket: "bikebox-app.appspot.com",
   messagingSenderId: "1080815309670",
   appId: "1:1080815309670:web:16ead4e6602816cf3d2ea7"
-};
+});
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getDatabase();
+// const db = getDatabase(app);
 
-export { db, ref, onValue };
+// export { db, firebaseConfig };
